e00ee42bf41e7326541de54465b8be8c87055106
Can HUANG <huang.volcano@gmail.com>
2016-04-17 21:17:36 +0100

    beat 61.72%

--- cpp/longestSubstringWithoutRepeatingCharaters.cpp
+++ cpp/longestSubstringWithoutRepeatingCharaters.cpp
@@ -2,24 +2,23 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         string subString = "";
-        int maxLength = 0;
+        int maxLength = 0, length = 0;
         for(auto it = s.begin(); it != s.end(); ++it)
             {
-                int length = 0;
-                if(subString.find(*it) == string::npos)
+                size_t pos = subString.find_first_of(*it);
+                subString.push_back(*it);
+                if(pos == string::npos)
                     {
-                        // subString.append(*it);
-                        subString += *it;
                         length ++;
+                        if(length > maxLength) maxLength = length;
                     }
                 else
                     {
-                        if(length > maxLength)
-                            {
-                                maxLength = length;
-                            }
-                        subString.clear();
+                        subString.erase(0, pos + 1);
+                        length -= pos;
                     }
             }
+
+            return maxLength;
     }
 };
