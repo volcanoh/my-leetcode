74e7913ab58d7dd147156540ff5f1cec2c2ee7db
Can HUANG <huang.volcano@gmail.com>
2016-07-28 20:30:41 +0800

    Update longestCommonPrefix.cpp

--- cpp/longestCommonPrefix.cpp
+++ cpp/longestCommonPrefix.cpp
@@ -1,23 +1,15 @@
-// bool compare(string a, string b) {
-//     return a.size() < b.size();
-// }
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         string result = "";
         if (strs.size() == 0) return result;
-//          sort(strs.begin(), strs.end(), compare);
-        result = *strs.begin();
-        while(result.size() != 0) {
-            bool inBreak = false;
-            for (vector<string>::iterator it = strs.begin(); it != strs.end(); ++it) {
-                if (it->find(result) != 0) {
-                    result = result.substr(0, result.size() - 1);
-                    inBreak = true;
-                    break;
-                }
-            }
-            if (!inBreak) break;
+        sort(strs.begin(), strs.end());
+        string begin = *strs.begin();
+        string end = *(strs.end() - 1);
+        int size = begin.size() < end.size() ? begin.size() : end.size();
+        for (int i = 0; i < size; ++ i) {
+            if (begin[i] == end[i]) result += begin[i];
+            else return result;
         }
         return result;
     }
